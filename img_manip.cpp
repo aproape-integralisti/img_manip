50f27d00575661fa1b2fbe8b09180194665e0a26
Bogdan Carp <carpbogdan01@gmail.com>
2021-03-30 21:26:18 +0300

    lower waiting time
    compressed 3 nested for and 2 for into only 2 for

--- img_manip.cpp
+++ img_manip.cpp
@@ -3,13 +3,17 @@
 
 int main()
 {
-    Image img_1("pic_20.jpg");
-  
-    cout << img_1;
+    Image img_1("pic_1.jpg");
+    Image img_2("pic_1.jpg");
+    Image img_3("pic_1.jpg");
 
-    img_1.imageCompress(32);
+    img_1.imageCompress(16);
+    img_2.imageCompress(16);
+    img_3.imageCompress(16);
 
     img_1.printImage();
-
+    img_2.printImage();
+    img_3.printImage();
+    
     return 0;
 }
--- kmeans.h
+++ kmeans.h
@@ -21,6 +21,8 @@ void debugshow2(vector<int>const& sume)
 	cout << endl;
 }
 
+
+
 void kMeansClustering(vector<vector<Pixel>>& pixels, int clusters) {
 	vector<Pixel> centroids;
 	int a = 0;
@@ -29,12 +31,15 @@ void kMeansClustering(vector<vector<Pixel>>& pixels, int clusters) {
 
 	auto dim_row = pixels.size();
 	auto dim_col = pixels[0].size();
+	
 
 	// Pick random centroids
 	for (int i = 0; i < clusters; i++) 
 	{
 		centroids.push_back(pixels[rand() % (dim_row)][rand() % (dim_col)]);
 	}
+
+	auto dim_centroids = centroids.size();
 
 	// Counting number of centroids to check if we must stop
 	int crt_centroids = 0;
@@ -45,47 +50,35 @@ void kMeansClustering(vector<vector<Pixel>>& pixels, int clusters) {
 	{
 		crt_centroids = 0;
 		// cout << "CENTROIDS MODIFIED ONCE" << '\n';
-		// Assign pixels to centroids
-		for (auto itr = centroids.begin(); itr != centroids.end(); itr++) 
-		{
-			int clusterId = itr - centroids.begin();
-
-			for (auto row = pixels.begin(); row != pixels.end(); row++) 
-			{
-				for (auto col = row->begin(); col != row->end(); col++) 
-				{
-					Pixel px = *col;
-					double dist = itr->distance(px);
-
-					if (dist < px.minDist) 
-					{
-						px.minDist = dist;
-						px.cluster = clusterId;
-					}
-
-					*col = px;
-				}
-			}
-		}
+		double dist, minDist;
+		Pixel* px;
 
 		// Initialise with zeroes
 		vector<int> nPixels(clusters, 0);
 		vector<double> sumR(clusters, 0.0), sumG(clusters, 0.0), sumB(clusters, 0.0);
 
-		for (auto row = pixels.begin(); row != pixels.end(); row++) 
+		for (int row = 0; row < dim_row; row++)
 		{
-			for (auto col = row->begin(); col != row->end(); col++) 
+			for (int col = 0; col < dim_col; col++)
 			{
-				int clusterId = col->cluster;
+				minDist = DBL_MAX;
+				px = &pixels[row][col];
 
-				Pixel px = *col;
+				for (int pos = 0; pos < dim_centroids; pos++)
+				{
+					dist = centroids[pos].distance(*px);
 
-				nPixels[clusterId] += 1;
-				sumR[clusterId] += px.r;
-				sumG[clusterId] += px.g;
-				sumB[clusterId] += px.b;
+					if(dist < minDist)
+					{
+						minDist = dist;
+						px->cluster = pos;
+					}
+				}
 
-				col->minDist = DBL_MAX;
+				nPixels[px->cluster] += 1;
+				sumR[px->cluster] += px->r;
+				sumG[px->cluster] += px->g;
+				sumB[px->cluster] += px->b;
 			}
 		}
 
